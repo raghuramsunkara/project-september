63aa1355ecdb17f6d68145c1384a8587ffc48c87
raghusunkara <raghusunkara18@gmail.com>
2026-09-25 15:16:32 +0530

    Add print statement for today's date

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ class HelloWorld{
     public static void main(String[] args) {
         System.out.println("hi Hello world.. its been a long time....im raghu the developer");
         System.out.println("In Main");
+        System.out.print("today is sept25th");
     }
 }
